668e83e84e5b266f84bbefbda06ca19bad9c227a
story <story801216@yahoo.com.tw>
2021-10-23 00:06:02 +0800

    忘記頁面css修改完成

--- src/components/zi/忘記密碼/forgetPassword.jsx
+++ src/components/zi/忘記密碼/forgetPassword.jsx
@@ -4,26 +4,26 @@ import Background from '../../../img/loginBackground.png'
 function ForgetPassword() {
   return (
     <>
-      <div class="zi-forgetPassword">
-        <img class="zi-forgetPassword-background" src={Background} alt="" />
-        <div class="zi-forgetPassword-card">
-          <div class="zi-forgetPassword-card-content">
+      <div className="zi-forgetPassword">
+        <img className="zi-forgetPassword-background" src={Background} alt="" />
+        <div className="zi-forgetPassword-card">
+          <div className="zi-forgetPassword-card-content">
             <h1>發送驗證信</h1>
             <p>請填寫註冊時的電子郵件</p>
-            <div class="zi-forgetPassword-card-line"></div>
-            <form class="zi-forgetPassword-form-content" action="">
+            <div className="zi-forgetPassword-card-line"></div>
+            <form className="zi-forgetPassword-form-content" action="">
               <label for="E-mail">電子郵件</label>
               <input
                 type="email"
                 id="E-mail"
-                class="Email-text"
+                className="Email-text"
                 placeholder="請輸入電子郵件"
               />
 
-              <div class="zi-forgetPassword-button-flex">
+              <div className="zi-forgetPassword-button-flex">
                 <button
                   type="submit"
-                  class="zi-forgetPassword-button-flex-verify-submit"
+                  className="zi-forgetPassword-button-flex-verify-submit"
                 >
                   發送驗證信
                 </button>
