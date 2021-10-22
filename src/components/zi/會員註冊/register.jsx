9cb94c86677f87d67dbaeca6668d957c09b86bb6
story <story801216@yahoo.com.tw>
2021-10-22 23:56:16 +0800

    會員註冊CSS修改完成

--- src/components/zi/會員註冊/register.jsx
+++ src/components/zi/會員註冊/register.jsx
@@ -4,47 +4,47 @@ import Background from '../../../img/loginBackground.png'
 function Register() {
   return (
     <>
-      <div class="zi">
-        <img class="background" src={Background} alt="" />
-        <div class="zi-card">
-          <div class="card-content">
+      <div className="zi-register">
+        <img className="zi-register-background" src={Background} alt="" />
+        <div className="zi-register-card">
+          <div className="zi-register-card-content">
             <h1>會員註冊</h1>
             <p>請填寫「欲至門市領藥的聯絡人」資料</p>
-            <div class="card-line"></div>
-            <form class="form-content" action="">
+            <div className="zi-register-card-line"></div>
+            <form className="zi-register-form-content" action="">
               <label for="E-mail">電子郵件</label>
               <input
                 type="email"
                 id="E-mail"
-                class="Email-text"
+                className="Email-text"
                 placeholder="請輸入電子郵件"
               />
               <label for="Password">密碼</label>
               <input
                 type="password"
                 id="Password"
-                class="Password-text"
+                className="zi-register-Password-text"
                 placeholder="請輸入密碼"
               />
               <label for="Name">姓名</label>
               <input
                 type="text"
                 id="Name"
-                class="Name-text"
+                className="zi-register-Name-text"
                 placeholder="請輸入姓名"
               />
               <label for="Birthday">生日</label>
               <input
                 type="date"
                 id="Birthday"
-                class="Birthday-text"
+                className="zi-register-Birthday-text"
                 placeholder="請輸入生日"
               />
               <label for="Id-number">身分證字號</label>
               <input
                 type="text"
                 id="Id-number"
-                class="Id-number"
+                className="zi-register-Id-number"
                 placeholder="請輸入身分證字號"
               />
 
@@ -52,7 +52,7 @@ function Register() {
               <input
                 type="text"
                 id="Mobile"
-                class="Mobile"
+                className="zi-register-Mobile"
                 placeholder="請輸入手機號碼"
               />
 
@@ -60,17 +60,17 @@ function Register() {
               <input
                 type="password"
                 id="Address"
-                class="Password-text"
+                className="zi-register-Address"
                 placeholder="請輸入聯絡地址"
               />
-              <div class="checkbox-flex">
-                <input type="checkbox" id="register-Checkbox" />
+              <div className="zi-register-checkbox-flex">
+                <input type="checkbox" id="zi-register-Checkbox" />
                 <p>
                   我已經閱讀 <a href="/">隱私權條款</a> 並已同意
                 </p>
               </div>
-              <div class="button-flex">
-                <button type="submit" class="register-submit">
+              <div className="zi-register-button-flex">
+                <button type="submit" className="zi-register-submit">
                   確認送出
                 </button>
               </div>
